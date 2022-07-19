9711cceb7a2150fad368f4f550506984a0c914f2
lipa71 <107254294+lipa71@users.noreply.github.com>
2022-07-19 16:42:16 +0200

    upload

---
+++ boxing timer/ap.js
@@ -0,0 +1,53 @@
+const opcje = document.getElementById('opcje');
+const liczba_rund = document.getElementById('liczba_rund');
+const czas_rundy = document.getElementById('czas_rundy');
+const czas_przerwy = document.getElementById('czas_przerwy');
+const runda = document.getElementById('runda');
+const timer = document.getElementById('timer');
+const start = document.getElementById('start');
+const stop = document.getElementById('stop');
+const czas_do_startu = document.getElementById('czas_do_startu');
+const ok = document.getElementById('ok');
+
+
+ok.addEventListener('click', (zmiana_opcji) => {
+    timer.innerText = czas_rundy.value
+});
+
+
+start.addEventListener('click', () => {
+    var set = czas_do_startu.value
+    var replaced = set.replace(/\D/g, '');
+    if (replaced !=='') {
+        var milisec = Number(replaced)
+        };
+    
+    var myArray = timer.innerText.split(':');
+    var min = myArray[0];
+    var sec = myArray[1];
+
+    var liczenie =setTimeout(function() {
+        var myInterval = setInterval(function(){
+                timer.innerText = min + ':' + sec ;   
+                if (sec === '00') {
+                    sec = 60
+                    min = '0' + (min - 1)
+                };
+                sec--
+                if (sec < 10) {
+                    sec = '0' + sec
+                };
+                if (sec === 0) {
+                    sec = 59
+                    min = '0' + (min - 1)
+                };
+                if (min === '00' && sec === '00') {
+                    clearInterval(myInterval)
+                    var koniec = setTimeout(function() {
+                        timer.innerText = '00:00'
+                    }, 1000);
+                };    
+                }, 1000);
+                
+    }, milisec * 1000);    
+});
