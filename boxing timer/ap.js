ebbdaad2b7df5dac59e50d8e4b5a3ffbef472979
lipa71 <lipa71sqad@gmail.com>
2022-07-19 21:06:13 +0200

    update

--- boxing timer/ap.js
+++ boxing timer/ap.js
@@ -8,6 +8,8 @@ const start = document.getElementById('start');
 const stop = document.getElementById('stop');
 const czas_do_startu = document.getElementById('czas_do_startu');
 const ok = document.getElementById('ok');
+var i = 0;
+
 
 
 ok.addEventListener('click', (zmiana_opcji) => {
@@ -16,11 +18,17 @@ ok.addEventListener('click', (zmiana_opcji) => {
 
 
 start.addEventListener('click', () => {
-    var set = czas_do_startu.value
-    var replaced = set.replace(/\D/g, '');
+    var x = czas_do_startu.value;
+    var replaced = x.replace(/\D/g, '');
     if (replaced !=='') {
         var milisec = Number(replaced)
         };
+
+    var y = czas_przerwy.value
+    var replaced = y.replace(/\D/g, '');
+    if (replaced !=='') {
+        var przerwa = Number(replaced)
+        };    
     
     var myArray = timer.innerText.split(':');
     var min = myArray[0];
@@ -46,8 +54,35 @@ start.addEventListener('click', () => {
                     var koniec = setTimeout(function() {
                         timer.innerText = '00:00'
                     }, 1000);
+                    var liczenie = setTimeout(function() {
+                        timer.innerText = czas_rundy.value
+                        var myArray2 = timer.innerText.split(':');
+                        var min2 = myArray2[0];
+                        var sec2 = myArray2[1];
+                        var myInterval = setInterval(function(){
+                            timer.innerText = min2 + ':' + sec2 ;   
+                               if (sec2 === '00') {
+                                    sec2 = 60
+                                    min2 = '0' + (min2 - 1)
+                                };
+                                sec2--
+                                if (sec2 < 10) {
+                                    sec2 = '0' + sec2
+                                };
+                                if (sec2 === 0) {
+                                    sec2 = 59
+                                    min2 = '0' + (min2 - 1)
+                                };
+                                if (min2 === '00' && sec2 === '00') {
+                                    clearInterval(myInterval)
+                                    var koniec = setTimeout(function() {
+                                        timer.innerText = '00:00'
+                                    }, 1000);
+                                };    
+                        }, 1000);            
+                    }, przerwa * 1000);
+
                 };    
                 }, 1000);
-                
     }, milisec * 1000);    
 });
