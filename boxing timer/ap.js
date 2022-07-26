9143144373f9ee3739ca0baf5f1e9ac72bdf6b9a
lipa71 <lipa71sqad@gmail.com>
2022-07-26 19:54:09 +0200

    :)

--- boxing timer/ap.js
+++ boxing timer/ap.js
@@ -1,40 +1,34 @@
 const opcje = document.getElementById("opcje");
-const liczba_rund = document.getElementById("liczba_rund");
-const czas_rundy = document.getElementById("czas_rundy");
-const czas_przerwy = document.getElementById("czas_przerwy");
-const runda = document.getElementById("runda");
+const number_of_rounds = document.getElementById("liczba_rund");
+const round_time = document.getElementById("czas_rundy");
+const break_time = document.getElementById("czas_przerwy");
+const round = document.getElementById("runda");
 const timer = document.getElementById("timer");
-const start = document.getElementById("start");
 const stop = document.getElementById("stop");
-const czas_do_startu = document.getElementById("czas_do_startu");
-const ok = document.getElementById("ok");
+const time_to_start = document.getElementById("czas_do_startu");
 
-ok.addEventListener("click", () => {
-  timer.innerText = czas_rundy.value;
-});
-
-start.addEventListener("click", () => {
-  var x1 = czas_do_startu.value;
+$("#ok").click(() => {
+  var x1 = time_to_start.value;
   var replaced1 = x1.replace(/\D/g, "");
   if (replaced1 !== "") {
     var start1 = Number(replaced1);
   }
 
-  var x2 = czas_przerwy.value;
+  var x2 = break_time.value;
   var replaced2 = x2.replace(/\D/g, "");
   if (replaced2 !== "") {
-    var przerwa = Number(replaced2);
+    var breakTime = Number(replaced2);
   }
-  var x3 = czas_rundy.value;
+  var x3 = round_time.value;
   var replaced3 = x3.replace(/\D/g, "");
   if (replaced3 !== "") {
-    var czasRundy = Number(replaced3);
+    var roundTime = Number(replaced3);
   }
 
-  var x4 = runda.textContent;
+  var x4 = round.textContent;
   var replaced4 = x4.replace(/\D/g, "");
   if (replaced4 !== "") {
-    var wskaznik_rundy = Number(replaced4);
+    var round_number = Number(replaced4);
   }
 
   var myArray = timer.innerText.split(":");
@@ -43,30 +37,36 @@ start.addEventListener("click", () => {
   const min2 = myArray[0];
   const sec2 = myArray[1];
 
-  var miedzyRundami = przerwa + czasRundy;
+  var bettwenRounds = breakTime + roundTime;
 
-  var liczenie = setInterval(() => {
-    var myInterval = setInterval(() => {
-      timer.innerText = min + ":" + sec;
-      if (min === "00" && sec === "00") {
-        clearInterval(myInterval);
-        wskaznik_rundy++;
-        runda.innerText = wskaznik_rundy + " Runda";
-        min = min2;
-        sec = sec2;
-      }
-      if (sec === "00") {
-        sec = 60;
-        min = "0" + (min - 1);
-      }
-      sec--;
-      if (sec < 10) {
-        sec = "0" + sec;
-      }
-      if (sec === 0) {
-        sec = 59;
-        min = "0" + (min - 1);
-      }
-    }, 1000);
-  }, miedzyRundami * 1000);
+  timer.innerText = round_time.value;
+
+  $("#start").click(() => {
+    setTimeout(() => {
+      var counting = setInterval(() => {
+        var myInterval = setInterval(() => {
+          timer.innerText = min + ":" + sec;
+          if (min === "00" && sec === "00") {
+            clearInterval(myInterval);
+            round_number++;
+            round.innerText = round_number + " Runda";
+            min = min2;
+            sec = sec2;
+          }
+          if (sec === "00") {
+            sec = 60;
+            min = "0" + (min - 1);
+          }
+          sec--;
+          if (sec < 10) {
+            sec = "0" + sec;
+          }
+          if (sec === 0) {
+            sec = 59;
+            min = "0" + (min - 1);
+          }
+        }, 1000);
+      }, bettwenRounds * 1000);
+    }, breakTime * 300);
+  });
 });
