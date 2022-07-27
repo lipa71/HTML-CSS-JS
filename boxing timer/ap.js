4e7dda86c745a62d5b13844c17f137f6561db769
lipa71 <lipa71sqad@gmail.com>
2022-07-27 21:54:27 +0200

    zmiana języka na angielski, poprawka zmiennych

--- boxing timer/ap.js
+++ boxing timer/ap.js
@@ -1,50 +1,52 @@
-const opcje = document.getElementById("opcje");
-const number_of_rounds = document.getElementById("liczba_rund");
-const round_time = document.getElementById("czas_rundy");
-const break_time = document.getElementById("czas_przerwy");
-const round = document.getElementById("runda");
+const options = document.getElementById("options");
+const number_of_rounds = document.getElementById("number_of_rounds");
+const round_time = document.getElementById("round_time");
+const break_time = document.getElementById("break_time");
+const round = document.getElementById("round");
 const timer = document.getElementById("timer");
 const stop = document.getElementById("stop");
-const time_to_start = document.getElementById("czas_do_startu");
+const time_to_start = document.getElementById("time_to_start");
+const ok = document.getElementById("ok");
+const start = document.getElementById("start");
 
-$("#ok").click(() => {
-  var x1 = time_to_start.value;
-  var replaced1 = x1.replace(/\D/g, "");
+ok.addEventListener("click", () => {
+  const x1 = time_to_start.value;
+  const replaced1 = x1.replace(/\D/g, "");
   if (replaced1 !== "") {
-    var start1 = Number(replaced1);
+    var startTime = Number(replaced1);
   }
 
-  var x2 = break_time.value;
-  var replaced2 = x2.replace(/\D/g, "");
+  const x2 = break_time.value;
+  const replaced2 = x2.replace(/\D/g, "");
   if (replaced2 !== "") {
     var breakTime = Number(replaced2);
   }
-  var x3 = round_time.value;
-  var replaced3 = x3.replace(/\D/g, "");
+  const x3 = round_time.value;
+  const replaced3 = x3.replace(/\D/g, "");
   if (replaced3 !== "") {
     var roundTime = Number(replaced3);
   }
 
-  var x4 = round.textContent;
-  var replaced4 = x4.replace(/\D/g, "");
+  const x4 = round.textContent;
+  const replaced4 = x4.replace(/\D/g, "");
   if (replaced4 !== "") {
     var round_number = Number(replaced4);
   }
 
-  var myArray = timer.innerText.split(":");
-  var min = myArray[0];
-  var sec = myArray[1];
-  const min2 = myArray[0];
-  const sec2 = myArray[1];
+  const min_and_sec = timer.innerText.split(":");
+  let min = min_and_sec[0];
+  let sec = min_and_sec[1];
+  const min2 = min_and_sec[0];
+  const sec2 = min_and_sec[1];
 
-  var bettwenRounds = breakTime + roundTime;
+  const bettwenRounds = breakTime + roundTime;
 
   timer.innerText = round_time.value;
 
-  $("#start").click(() => {
+  start.addEventListener("click", () => {
     setTimeout(() => {
-      var counting = setInterval(() => {
-        var myInterval = setInterval(() => {
+      const counting = setInterval(() => {
+        const myInterval = setInterval(() => {
           timer.innerText = min + ":" + sec;
           if (min === "00" && sec === "00") {
             clearInterval(myInterval);
@@ -67,6 +69,6 @@ $("#ok").click(() => {
           }
         }, 1000);
       }, bettwenRounds * 1000);
-    }, breakTime * 300);
+    }, startTime * 300);
   });
 });
